dc90460484fc10d9349b9cb990062aaf63de2e70
WhoDis-MaName <andrawilliams03.cs@gmail.com>
2023-03-19 21:22:50 -0500

    Add connection Files
    Connection is successful prior to commit. Need to work on connecting html to java.

---
+++ SplitAudio/src/main/java/DBConnection.java
@@ -0,0 +1,60 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletContext;
+
+public class DBConnection {
+   static Connection connection = null;
+   static ServletContext servletContext;
+
+   static void getDBConnection() {
+      System.out.println("-------- MySQL JDBC Connection Testing ------------");
+      try {
+         Class.forName("com.mysql.jdbc.Driver");
+      } catch (ClassNotFoundException e) {
+         System.out.println("Where is your MySQL JDBC Driver?");
+         e.printStackTrace();
+         return;
+      }
+      System.out.println("MySQL JDBC Driver Registered!");
+
+      connection = null;
+      try {
+         UtilProp.loadProperty(servletContext);
+         connection = DriverManager.getConnection(getURL(), getUserName(), getPassword());
+      } catch (Exception e) {
+         System.out.println("Connection Failed! Check output console");
+         e.printStackTrace();
+         return;
+      }
+
+      if (connection != null) {
+         System.out.println("You made it, take control your database now!");
+      } else {
+         System.out.println("Failed to make connection!");
+      }
+   }
+
+   static String getURL() {
+      String url = UtilProp.getProp("url");
+      System.out.println("[DBG] URL: " + url);
+      return url;
+   }
+
+   static String getUserName() {
+      String usr = UtilProp.getProp("user");
+      System.out.println("[DBG] URL: " + usr);
+      return usr;
+   }
+
+   static String getPassword() {
+      String pwd = UtilProp.getProp("password");
+      System.out.println("[DBG] URL: " + pwd);
+      return pwd;
+   }
+
+   public static void getDBConnection(ServletContext context) {
+      servletContext = context;
+      getDBConnection();
+   }
+}
