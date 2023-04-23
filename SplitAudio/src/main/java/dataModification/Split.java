b2f69715fff6ec6ef1310afe20e9a074917fffff
Hello0801World <shun19036@yahoo.co.jp>
2023-04-23 14:58:58 -0500

    Implemented split and trim feature

--- SplitAudio/src/main/java/dataModification/Split.java
+++ SplitAudio/src/main/java/dataModification/Split.java
@@ -1,11 +1,19 @@
  package dataModification;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
  * Servlet implementation class Split
@@ -26,8 +34,43 @@ public class Split extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String filename = request.getParameter("audio-file").trim();
+		String segmentDuration = request.getParameter("splitWhen").trim();
+		
+		System.out.println(filename);
+		
+		int segmentDurationInSeconds = Integer.parseInt(segmentDuration);
+		
+		System.out.println(segmentDurationInSeconds);
+		try {
+			File inputFile = new File(filename);
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(inputFile);
+	        AudioFormat format = audioInputStream.getFormat();
+
+	        int bytesPerSecond = format.getFrameSize() * (int)format.getFrameRate();
+//	        int segmentDurationInSeconds;
+			int segmentLength = bytesPerSecond * segmentDurationInSeconds;
+
+	        int segmentNumber = 1;
+	        int bytesRead = 0;
+	        byte[] audioBuffer = new byte[segmentLength];
+
+	        while ((bytesRead = audioInputStream.read(audioBuffer)) != -1) {
+	            File outputFile = new File(inputFile.getParentFile(), "segment_" + segmentNumber + ".wav");
+	            AudioInputStream segmentAudioInputStream = new AudioInputStream(audioInputStream, format, bytesRead);
+	            AudioSystem.write(segmentAudioInputStream, AudioFileFormat.Type.WAVE, outputFile);
+	            segmentNumber++;
+	        }
+	        
+	        audioInputStream.close();
+			
+		}
+		catch (UnsupportedAudioFileException e){
+			System.out.println(e);
+		}
+		catch (IOException e) {
+			
+		}
 	}
 
 	/**
