19939b626c8d549d35d022ffda2e2fa0f6c84691
langnjames <you@example.com>
2023-05-01 20:43:22 -0500

    Uploading audio files should upload to a directory specified by the user in the Upload.java file.

--- SplitAudio/src/main/java/Upload.java
+++ SplitAudio/src/main/java/Upload.java
@@ -2,7 +2,7 @@
 
 import java.io.File;
 import java.io.IOException;
-
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -33,22 +33,55 @@ public class Upload extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		try {
-		Part filePart = request.getPart("audio-file");
-		String filename = filePart.getSubmittedFileName();
-		File file = new File (filename);
-		UtilDB.getSession().setFile(file);
-		}
-		catch(Exception e) {
-			System.out.println("Erorrrrrr");
-		}
-		
-		response.sendRedirect(request.getContextPath()+"/EditPage.html");
-		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        
+    	
+    	try {
+        	
+        	
+        	// Get the uploaded file from the request
+            Part filePart = request.getPart("audio-file");
+            String filename = filePart.getSubmittedFileName();
+            
+        
+            
+            // Get the file extension
+            String extension = "";
+            int i = filename.lastIndexOf('.');
+            if (i > 0) {
+                extension = filename.substring(i + 1);
+            }
+
+            // Check if the file is an audio file
+            if (!extension.equalsIgnoreCase("wav")) {
+            	response.sendRedirect(request.getContextPath() + "/EditPage.html?success=badFileExt");
+                return;
+            }
+            
+            // Get the file size
+            long fileSize = filePart.getSize();
+
+            // Check if the file size is within acceptable limits
+            if (fileSize > 10000000) { // 10 MB
+            	response.sendRedirect(request.getContextPath() + "/EditPage.html?success=badFileSize");
+                return;
+            }
+            
+            
+            File file = new File("C:\\audio_files\\" + filename); // Modify this path to match the directory where you want to save the file
+            filePart.write(file.getAbsolutePath());
+            
+         // Redirect back to the form with a success message
+            response.sendRedirect(request.getContextPath() + "/EditPage.html?success=true");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
