e4847f2bd1043153ca7beca30231ec51e2fe7fc6
danielmulangu <76424137+danielmulangu@users.noreply.github.com>
2023-05-02 19:27:32 -0500

    Updated Login

---
+++ webproject/src/main/java/Home2.java
@@ -0,0 +1,205 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import datamodel.*;
+import util.*;
+
+@WebServlet("/Home2")
+public class Home2 extends HttpServlet implements Info {
+   private static final long serialVersionUID = 1L;
+
+   public Home2() {
+      super();
+   }
+
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+		   throws ServletException, IOException {
+      String option = request.getParameter("options").trim();
+
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+      //String title = "Messages";
+      String docType = "<!doctype html>"; //
+      out.println(docType + //
+            "\r\n"
+            + "<html>\r\n"
+            + "	<head>\r\n"
+            + "		<style>\r\n"
+            + "		.loginbox p{\r\n"
+            + "			margin: 0;\r\n"
+            + "			padding: 0;\r\n"
+            + "			font-weight: bold;\r\n"
+            + "		}\r\n"
+            + "		\r\n"
+            + "		.loginbox {\r\n"
+            + "			width: 400px;\r\n"
+            + "			height: auto;\r\n"
+            + "			background: #ffffff;\r\n"
+            + "			color: #000000;\r\n"
+            + "			top: 50%;\r\n"
+            + "			left: 50%;\r\n"
+            + "			position: relative;\r\n"
+            + "			transform: translate(-50%, 2.75%);\r\n"
+            + "			box-sizing: border-box;\r\n"
+            + "			padding: 50px 50px;\r\n"
+            + "		}\r\n"
+            + "		\r\n"
+            + "		.login{\r\n"
+            + "			width: 100px;\r\n"
+            + "			height: 100px;\r\n"
+            + "			border-radius: 50%;\r\n"
+            + "			position: absolute;\r\n"
+            + "			top: -50px;\r\n"
+            + "			left: calc(50% - 50px);\r\n"
+            + "		\r\n"
+            + "		\r\n"
+            + "		}\r\n"
+            + "		\r\n"
+            + "		.loginbox input{\r\n"
+            + "			width: 100%;\r\n"
+            + "			margin-bottom: 20px;\r\n"
+            + "		}\r\n"
+            + "		\r\n"
+            + "		.loginbox input[type=\"text\"], input[type=\"password\"], input[type=\"file\"]\r\n"
+            + "		{\r\n"
+            + "			border: none;\r\n"
+            + "			border-bottom: 1px solid #909090;\r\n"
+            + "			background: transparent;\r\n"
+            + "			outline: none;\r\n"
+            + "			height: 40px;\r\n"
+            + "			color: #000000;\r\n"
+            + "			font-size: 14px;\r\n"
+            + "		}\r\n"
+            + "		\r\n"
+            + "		.loginbox button[type=\"submit\"]\r\n"
+            + "		{\r\n"
+            + "			border: none;\r\n"
+            + "			outline: none;\r\n"
+            + "			height: 40px;\r\n"
+            + "			background: #909090;\r\n"
+            + "			color: #ffffff;\r\n"
+            + "			font-size: 18px;\r\n"
+            + "			border-radius: 20px;\r\n"
+            + "		}\r\n"
+            + "		\r\n"
+            + "		.loginbox button[type=\"submit\"]:hover\r\n"
+            + "		{\r\n"
+            + "			cursor: pointer;\r\n"
+            + "			background: #909090;\r\n"
+            + "			color: #000000;\r\n"
+            + "		}\r\n"
+            + "		\r\n"
+            + "		.loginbox a{\r\n"
+            + "			text-decoration: none;\r\n"
+            + "			font-size: 12px;\r\n"
+            + "			line-height: 18px;\r\n"
+            + "			color: white;\r\n"
+            + "		}\r\n"
+            + "		\r\n"
+            + "		.loginbox a:hover\r\n"
+            + "		{\r\n"
+            + "			color: #404040;\r\n"
+            + "		}\r\n"
+            + "		.main-component {\r\n"
+            + "			margin-left: 17%;\r\n"
+            + "			padding: 20px;\r\n"
+            + "		}\r\n"
+            + "		\r\n"
+            + "		h3 {\r\n"
+            + "			font-size: 30px;\r\n"
+            + "		}\r\n"
+            + "		\r\n"
+            + "		 .input-trim {\r\n"
+            + "		 	margin: 5px;\r\n"
+            + "		 }\r\n"
+            + "		 \r\n"
+            + "		 label {\r\n"
+            + "		 	font-size: 20px;\r\n"
+            + "		 }\r\n"
+            + "		 \r\n"
+            + "		 input {\r\n"
+            + "		 	font-size: 20px;\r\n"
+            + "		 }\r\n"
+            + "	</style>\r\n"
+            + "		<meta charset=\"ISO-8859-1\">\r\n"
+            + "		<link rel=\"stylesheet\" href=\"SplitAudio.css\">\r\n"
+            + "		<title>Files</title>\r\n"
+            + "		<link rel=\"icon\" type=\"image/x-icon\" href=\"Sound_Splice_Icon.svg\">\r\n"
+            + "	</head>\r\n"
+            + "<body>\r\n"
+            + "	<header>\r\n"
+            + "		<div id=\"page_wrapper\">\r\n"
+            + "		<img src=\"Sound_Splice_Logo.svg\" height=\"200\" width=\"150\"/>\r\n"
+            + "			<nav>	\r\n"
+            + "				<ul>\r\n"
+            + "					<!--Home  Add  Edit Account-->\r\n"
+            + "					<li><a href=\"Home2.html\">Home</a></li>\r\n"
+            + "					<li><a href=\"Files.html\">Files</a></li>\r\n"
+            + "					<li><a href=\"EditPage.html\">Edit</a></li>\r\n"
+            + "					<li><a href=\"Details()\">Account</a></li>\r\n"
+            + "				</ul>\r\n"
+            + "			</nav>\r\n"
+            + "		</div>\r\n"
+            + "	</header>\r\n"
+            + "	<div id=\"page_wrapper\">");
+      out.println("");
+      if(option.equals("my")) {
+    	  User user = UtilDB.getSession().getCurrentUser();
+    	  if(user == null) {
+    		  out.println("Log in to see your messages <br>");
+    		  out.println("<a href=/" + projectName + "/" + logIn + ">Login</a> <br>"); 
+    	  }else {
+    		  System.out.println("Made it to my files.");
+    		  List<Audio_Files> listMessages = null;
+ 		     listMessages = UtilDB.listFilesByUser(user);
+ 		     
+ 		     display_files(listMessages, out);
+    	  }
+      }else{
+    	  System.out.println("Made it to public files.");
+    	  List<Audio_Files> listMessages = null;
+		  listMessages = UtilDB.listFiles();
+	      display_files(listMessages, out);
+      }
+      
+      out.println("");
+      out.println("</div>");
+      out.println("</body></html>");
+   }
+
+   void display_files(List<Audio_Files> listMessages, PrintWriter out) {
+      out.println("<form action=\"UpdateFiles\" method=\"POST\">");
+	   for (Audio_Files file : listMessages) {
+         System.out.println("[DBG] " + file.getId() + ", " //
+               + UtilDB.userNameById(file.getuid()) + ", " //
+               + file.getName());
+
+         out.println("\n<div class=\"loginbox\">" + file.getName() + ". Made by: "
+                 + UtilDB.userNameById(file.getuid()) + ". Located at: "
+                 + file.getFilepath() 
+                 + "<br>"
+                 + "<div>"
+                 + "<button type:\"submit\" name = \"select\" value = \""+ file.getName()+"\"/>Select"
+                 + "<button type:\"submit\" name = \"delete\" value = \""+ file.getName()+"\"/>Delete"
+                 + "</div>"
+                 + "</div>"
+                 + "<br>"
+                 );
+      }
+	  out.println("</form>");
+   }
+
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+		   throws ServletException, IOException {
+      doGet(request, response);
+   }
+}
--- webproject/src/main/java/Login.java
+++ webproject/src/main/java/Login.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -14,65 +13,41 @@ import util.UtilDB;
 
 @WebServlet("/Login")
 public class Login extends HttpServlet implements Info {
-   private static final long serialVersionUID = 1L;
+	// Serial version UID for serialization/deserialization
+	private static final long serialVersionUID = 1L;
 
-   public Login() {
-      super();
-   }
+	// Constructor
+	public Login() {
+		super();
+	}
 
-   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      String username = request.getParameter("username").trim();
-      String password = request.getParameter("password").trim();
+	// Handle GET request
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Get the username and password parameters from the request
+		String username = request.getParameter("username").trim();
+		String password = request.getParameter("password").trim();
+		
+		// Check if the username and password are valid and exist in the database
+		List<User> listUsers = null;
+		if (username != null && !username.isEmpty() && password != null && !password.isEmpty()) {
+			listUsers = UtilDB.logInUser(username, password);
+			if(listUsers.isEmpty()) { // User not found
+				response.sendRedirect(request.getContextPath() + "/LogIn.html?success=noUser");
+			} else { // User found
+				// Set the session user to the first user in the list (assumes only one user with that username)
+				UtilDB.getSession().setUser(listUsers.get(0));
+				UtilDB.getSession();
+				// Redirect to the login page with a success message
+				response.sendRedirect(request.getContextPath() + "/Home2.html?success=goodLogin");
+			}
+		} else { // Username or password not provided
+			response.sendRedirect(request.getContextPath() + "/LogIn.html?success=badUserPass");
+		}
+	}
 
-      response.setContentType("text/html");
-      PrintWriter out = response.getWriter();
-      String title = "Database Result";
-      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n"; //
-      out.println(docType + //
-            "<html>\n" + //
-            "<head><title>" + title + "</title></head>\n" + //
-            "<body bgcolor=\"#f0f0f0\">\n" + //
-            "<h1 align=\"center\">" + title + "</h1>\n");
-      out.println("<ul>");
-
-      List<User> listUsers = null;
-      if (username != null && !username.isEmpty() && password != null && !password.isEmpty()) {
-         listUsers = UtilDB.logInUser(username,password);
-         if(listUsers.isEmpty()) {
-        	out.println("<h1>There is no user with that login.</h1> <br>");
-            out.println("<a href=/" + projectName + "/" + logIn + ">Log In</a> <br>");
-         }else {
-        	 UtilDB.getSession().setUser(listUsers.get(0));
-        	 display(listUsers, out);
-        	 System.out.print(UtilDB.getSession().getCurrentUser());
-        	 out.println("<a href=/" + projectName + "/" + home + ">Home</a> <br>");
-         }
-      } else {
-         //listEmployees = UtilDBWilliams.listUsers();
-         out.println("<h1>There must be a username and password entered.</h1> <br>");
-         out.println("<a href=/" + projectName + "/" + logIn + ">Log In</a> <br>");
-      }
-      
-      out.println("</ul>");
-      //out.println("<a href=/" + projectName + "/" + newMessage + ">Create Message</a> <br>");
-      //out.println("<a href=/" + projectName + "/" + searchMessage + ">View Message</a> <br>");
-      out.println("</body></html>");
-   }
-
-   void display(List<User> listUsers, PrintWriter out) {
-      for (User employee : listUsers) {
-         System.out.println("[DBG] " + employee.getId() + ", " //
-               + employee.getName() + ", " //
-               + employee.getEmail());
-
-         out.println("<li>" + employee.getId() + ", " //
-               + employee.getName() + ", " //
-               + employee.getEmail() + "</li>");
-      }
-   }
-
-   protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-		   throws ServletException, IOException {
-      doGet(request, response);
-   }
+	// Handle POST request
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
